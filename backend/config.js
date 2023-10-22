25552886f8be442224e03f953b26bc70b7eaf598
nawabsahab16 <nawab162004@gmail.com>
2023-10-23 00:17:14 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://nawab162004:nawab162004@college-project.s6cy6rg.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express, { response } from 'express';
+import { PORT , mongoDBURL} from './config.js';
+import mongoose from 'mongoose';
+
+
+const app = express();
+
+app.get('/' , (request, response) => {
+    console.log(request)
+    return response.status(234).send("Welcome to My HOME ")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening to Port : ${PORT}`);
+})
